73937f0ecad2dad1c81b586a756c6b3279b4a2a6
Oleg_Skalskiy <60063433+Oleg-Skalskiy@users.noreply.github.com>
2023-06-25 23:20:21 +0300

    HW1

---
+++ HW1/Task_10/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+Задача 10: Напишите программу, которая на вход принимает трехзначное число, а на выходе показывает последнюю цифру этого числа
+*/
+Console.Clear();
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 100 && number < 1000)
+{
+    int last = (number % 10);
+    Console.Write($"{last} - это последняя цифра в числе {number}");
+}
+else
+Console.Write("Это не трёхзначное число");
