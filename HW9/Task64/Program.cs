a12d10cf55a147e42d6044e55c5f2344120afb57
Oleg_Skalskiy <aist@oleg-skalskiy.ru>
2023-07-26 14:53:58 +0300

    HW9

---
+++ HW9/Task64/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите число N: ");
+int n = Convert.ToInt32(ReadLine());
+
+WriteLine($"N = {n} -> {PrintNumber(n, 2)}");
+string PrintNumber(int end, int start)
+{
+    if (start == end) return n.ToString();
+    return (n + ", " + PrintNumber(n--, start));
+}
---
+++ HW9/Task66/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите число M: ");
+int m = Convert.ToInt32(ReadLine());
+
+Write("Введите число N: ");
+int n = Convert.ToInt32(ReadLine());
+
+void SumNum(int m, int n, int sum)
+{
+    if (m > n)
+    {
+        Console.WriteLine($"Сумма натуральных элементов -> {sum}");
+        return;
+    }
+    sum = sum + (m++);
+    SumNum(m, n, sum);
+}
+SumNum(m, n, 0);
